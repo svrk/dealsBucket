3b3dc71fd1a36034202a3bea89974aab2061b3da
ram-vm <ram-vm@ultivoz.com>
2013-12-30 07:40:33 +0800

    enhancement in preparing response_json

--- api/deals.py
+++ api/deals.py
@@ -4,64 +4,34 @@ from config import UVConfig
 from cache_server import UVCache
 from json_util import JsonParser
 
-conf = UVConfig()
-conf.init("/home/uvadmin/ucp/conf/deals.conf")
-
 @singleton
 class UVDeals(object):
   """ This class used for getting deals """
 
   def get_deals(self):
- 
-#    deals_list = eval(UVCache().get('deals'))
-#    json_deals = JsonParser().to_json(deals_list) 
-   
-#    return json_deals 
+    #TODO
     pass  
+ 
+
+  def get_deals_by_category(self,l_country, l_city, l_category=''):
+    redis_key_1 = l_country + '_'  + l_city
+    redis_key_2 = l_category    
+    response_json = {}       
+    deals = UVCache().hget(redis_key_1, redis_key_2)
+    if deals == None:
+      deals = ''
+    response_json['deals'] = list(eval(deals))
+    return response_json  
 
 
-  def get_deals_by_division(self, division_id=''):
-    
-    #deal_api_name = conf.get_config_value("deals","apis")
+  def get_deals_by_country_and_city(self, l_country = '', l_city = ''):
+    response_json = {}       
+    deals = UVCache().hget(l_country, l_city)   
+    if deals == None:
+      deals = '' 
+    response_json['deals'] = list(eval(deals))
 
-    #TODO add deals from different deal api's
-    deals = UVCache().hget('groupon_deals', division_id)
-    return deals  
-
-
-  def get_deals_by_country_and_city(self, country = '', city = ''):
-
-    #deal_api_name = conf.get_config_value("deals","apis")
-
-    #TODO add deals from different deal api's
-    deals = UVCache().hget(country, city)
-    return deals
-
-
-
-
-
-#class Deal(object):
-
-#  def __init__(self):
-#    self.price = '10'
-#    self.websiteurl = 'www.groupon.com'
-#    self.discount = '5'
-
-#  def set_price(self, price):
-#    self.price = price
-#  def set_websiteurl(self, websiteurl):
-#    self.websiteurl = websiteurl
-#  def set_discount(self, discount):
-#    self.discount = discount
-
-#  def get_price(self):
-#    return self.price
-#  def get_websiteurl(self):
-#    return self.websiteurl
-#  def get_discount(self):
-#    return self.discount
-
+    return response_json
 
 
 
